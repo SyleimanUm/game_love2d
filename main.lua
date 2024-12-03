1e1ba8de4479a7a45751c9e4dbc99b34751afaf3
herthama <herthama@student.21-school.ru>
2024-12-03 20:31:02 +0300

    finish running jack

--- main.lua
+++ main.lua
@@ -1,60 +1,77 @@
+---@diagnostic disable-next-line: lowercase-global
 _G.love = require("love")
 
 function love.load()
-    love.graphics.setBackgroundColor(0.5, 0.5, 1)
+jack = {
+        x = 0,
+        y = 0,
+        sprite = love.graphics.newImage("img/run.png"),
 
-    _G.pacman = {}
-
-    pacman.x = 200
-    pacman.y = 250
-    pacman.angle1 = 0.5
-    pacman.angle2 = 5.5
-
-    _G.food = {
-        x = 600,
-        eaten = false
+        animation = {
+            dirction = "right",
+            idle = true,
+            frame = 1,
+            max_frames = 8,
+            speed = 30,
+            timer = 0.1
+        }
     }
+SPRITE_WIDTH, SPRITE_HEIGHT = 5352, 569
+QUAD_WIDTH = 669
+QUAD_HEIGTH = SPRITE_HEIGHT
+
+
+quads = {}
+
+for i = 1, jack.animation.max_frames  do
+    quads[i] = love.graphics.newQuad(QUAD_WIDTH * (i - 1), 0, QUAD_WIDTH, QUAD_HEIGTH, SPRITE_WIDTH, SPRITE_HEIGHT)
+end
+
 end
 
 function love.update(dt)
-    -- pacman.x = pacman.x + 1
-    
-    if love.keyboard.isDown("down") then
-        pacman.angle1 = pacman.angle1 + math.pi * dt
-        pacman.angle2 = pacman.angle2 + math.pi * dt
-        
-    elseif love.keyboard.isDown("up") then
-            pacman.angle1 = pacman.angle1 - math.pi * dt
-            pacman.angle2 = pacman.angle2 - math.pi * dt
-    end
 
-    
     if love.keyboard.isDown("d") then
-        pacman.x = pacman.x + 1
-    end
-    if love.keyboard.isDown("a") then
-            pacman.x = pacman.x - 1
-    end
-    if love.keyboard.isDown("w") then
-            pacman.y = pacman.y - 1
-    end
-    if love.keyboard.isDown("s") then
-            pacman.y = pacman.y + 1
+        jack.animation.idle = false
+        jack.animation.dirction = "right"
+    elseif love.keyboard.isDown("a") then
+        jack.animation.idle = false
+        jack.animation.dirction = "left"
+    else
+        jack.animation.idle = true
+        jack.animation.frame = 1
     end
 
 
-    if pacman.x >= food.x +20 then
-        food.eaten = true
+    if not jack.animation.idle then
+        jack.animation.timer = jack.animation.timer + dt
+
+        if jack.animation.timer > 0.2 then
+            jack.animation.timer = 0.1
+
+            jack.animation.frame = jack.animation.frame + 1
+
+            if jack.animation.dirction == "right" then
+                jack.x = jack.x + jack.animation.speed
+            elseif jack.animation.dirction == "left" then
+                jack.x = jack.x - jack.animation.speed
+            end
+
+            if jack.animation.frame > jack.animation.max_frames then
+                jack.animation.frame = 1
+            end
+            
+        end
     end
+
 end
 
 function love.draw()
-
-    if not food.eaten then
-        love.graphics.setColor(1, 0, 0)
-        love.graphics.rectangle("fill", food.x, 205, 30, 30)
+    love.graphics.scale(0.3)
+    
+    if jack.animation.dirction == "right" then
+        love.graphics.draw(jack.sprite, quads[jack.animation.frame], jack.x, jack.y)
+    else 
+        love.graphics.draw(jack.sprite, quads[jack.animation.frame], jack.x, jack.y, 0, -1, 1, QUAD_WIDTH + 100, 0)
     end
-
-    love.graphics.setColor(1, 0.5, 0)
-    love.graphics.arc("fill", pacman.x, pacman.y, 70, pacman.angle1, pacman.angle2)
-end
+end 
