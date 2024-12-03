e41f612509021659e06370995e84d25dc40868f5
herthama <herthama@student.21-school.ru>
2024-12-03 17:14:43 +0300

    pacman

---
+++ conf.lua
@@ -0,0 +1,14 @@
+function love.conf(t)
+    -- t.identity = "data/saves"
+    -- t.version = "3.5"
+    -- t.console = true
+    -- t.externalstorage = true
+    -- t.gammacorrect = true
+
+    -- t.audio.mic = true
+
+    t.window.title = "Cool ball game"
+    t.window.icon = "icon/game_icon.png"
+    t.window.width = 1200
+    t.window.height = 700
+end
